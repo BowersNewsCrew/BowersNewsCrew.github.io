9423ccea71acb27b5a740688766dc0484fd50657
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-09-29 14:03:01 -0500

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 var schedule = document.getElementsByClassName("schedule")[0];
 
 window.onscroll = function () {
-    if (window.pageYOffset > 500) {
+    if (window.pageYOffset > 160) {
       schedule.classList.remove("blur");
     }
 };
