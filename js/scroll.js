a2692be4fa70a65e796c41fa94bce8c2fe1ccfd0
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-09-29 14:31:09 -0500

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 var container = document.getElementsByClassName("container")[0];
 
 window.onscroll = function () {
-    container.style.transform = "scale("+ window.scrollY * 1.001 +")";
+    container.style.transform = "scale("+ (window.scrollY / 100) * 1.04 +")";
     //alert(window.scrollY);
 };
