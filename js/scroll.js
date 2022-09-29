4c8f019fc4aa5f512e0fda106dd3809902323a44
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-09-29 14:20:27 -0500

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 var container = document.getElementsByClassName("container")[0];
 
 window.onscroll = function () {
-    if (window.scrollY >= 40) {
+    if (window.scrollY >= 20) {
       container.classList.remove("blur");
     }
     //alert(window.scrollY);
